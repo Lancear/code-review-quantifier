95663ef00eed083574066dc583497cc7340bbdfa
Lance J <lancear153@gmail.com>
2023-04-27 23:13:01 +0200

    first baby steps

--- middleware.js
+++ middleware.js
@@ -8,6 +8,14 @@ export const config = {
  * @param {import('@vercel/edge').RequestContext} context 
  */
 export default function middleware(request, context) {
-  console.log(request.url);
-  return request;
+  const url = new URL(request.url);
+  console.log(url.pathname);
+
+  if (url.pathname === "/auth/login") {
+    const { CLIENT_ID } = process.env;
+    console.log(CLIENT_ID);
+    return Response.redirect('https://github.com/login/oauth/authorize?client_id=' + CLIENT_ID);
+  }
+
+  return new Response("simple middleware");
 };
