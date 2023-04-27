4b8bef4c1e953a674b93ab41fbb5f7d11ec1aa29
Lance J <lancear153@gmail.com>
2023-04-28 00:05:06 +0200

    request access token

--- middleware.js
+++ middleware.js
@@ -2,6 +2,20 @@ export const config = {
   matcher: ['/auth/:path*', '/api/:path*'],
 };
 
+async function fetchAccessToken(code, tokenInfo) {
+  const { CLIENT_ID, CLIENT_SECRET } = process.env;
+  const res = await fetch(
+    'https://github.com/login/oauth/access_token?client_id=' + CLIENT_ID + '&client_secret=' + CLIENT_SECRET + '&code=' + code, 
+    { method: 'POST', headers: { 'Accept': 'application/json' }}
+  );
+
+  Object.assign(tokenInfo, await res.json());
+}
+
+const STATE_LENGTH = 10;
+const CHECK_SUM_INDEX = 3;
+const CHECK_SUM_INIT = 13;
+
 /**
  * 
  * @param {Request} request 
@@ -10,29 +24,42 @@ export const config = {
 export default function middleware(request, context) {
   const url = new URL(request.url);
 
-  if (url.pathname === "/auth/login") {
-    const randomNumbers = new Uint8Array({ length: 10 });
-    crypto.getRandomValues(randomNumbers);
-
-    randomNumbers[9] = 13;
-    for (let idx = 0; idx < randomNumbers.length - 1; idx++) {
-      randomNumbers[9] += randomNumbers[idx] * idx;
+  try {
+    if (url.pathname === "/auth/login") {
+      const randomNumbers = new Uint8Array({ length: STATE_LENGTH });
+      crypto.getRandomValues(randomNumbers);
+  
+      randomNumbers[CHECK_SUM_INDEX] = CHECK_SUM_INIT;
+      for (let idx = 0; idx < randomNumbers.length; idx++) {
+        if (idx === CHECK_SUM_INDEX) continue;
+        randomNumbers[CHECK_SUM_INDEX] += randomNumbers[idx] * idx;
+      }
+  
+      const state = btoa(new TextDecoder().decode(randomNumbers));
+      const { CLIENT_ID } = process.env;
+      return Response.redirect('https://github.com/login/oauth/authorize?client_id=' + CLIENT_ID + "&state=" + state);
     }
-
-    const state = btoa(new TextDecoder().decode(randomNumbers));
-    const { CLIENT_ID } = process.env;
-    return Response.redirect('https://github.com/login/oauth/authorize?client_id=' + CLIENT_ID + "&state=" + state);
+    else if (url.pathname === "/auth/authorize") {
+      const stateNumbers = new TextEncoder().encode(atob(url.searchParams.get('state')));
+      if (stateNumbers.length > 2 * STATE_LENGTH) return new Response("Unauthorized", { status: 401 });
+  
+      let checkSum = CHECK_SUM_INIT;
+      for (let idx = 0; idx < stateNumbers.length - 1; idx++) {
+        if (idx === CHECK_SUM_INDEX) continue;
+        checkSum += parseInt(stateNumbers[idx]) * idx;
+      }
+  
+      const validState = stateNumbers.length === STATE_LENGTH && parseInt(stateNumbers[CHECK_SUM_INDEX]) === checkSum;
+      if (!validState) return new Response("Unauthorized", { status: 401 });
+  
+      const tokenInfo = {};
+      context.waitUntil(fetchAccessToken(url.searchParams.get('code'), tokenInfo));
+      console.log(JSON.stringify(tokenInfo, null, 2));
+      return new Response("simple middleware");
+    }
   }
-  else if (url.pathname === "/auth/authorize") {
-    const stateNumbers = new TextEncoder().encode(atob(url.searchParams.get('state')));
-
-    let checkSum = 13;
-    for (let idx = 0; idx < stateNumbers.length - 1; idx++) {
-      checkSum += parseInt(stateNumbers[idx]) * idx;
-    }
-
-    const validState = parseInt(stateNumbers[9]) === checkSum;
-    return validState ? new Response("oha vip guest") : new Response("simple middleware");
+  catch (err) {
+    return new Response("Unauthorized", { status: 401 });
   }
 
   return new Response("simple middleware");
