83bf3a32cd13e5d40d84195339957c6f439c9605
Lance J <lancear153@gmail.com>
2023-04-27 23:37:28 +0200

    state validation

--- middleware.js
+++ middleware.js
@@ -20,9 +20,23 @@ export default function middleware(request, context) {
       sum += randomNumbers[idx] * idx;
     }
 
-    const state = btoa(randomNumbers.join('') + sum);
+    const state = btoa(randomNumbers.join('.') + '.' + sum);
     const { CLIENT_ID } = process.env;
     return Response.redirect('https://github.com/login/oauth/authorize?client_id=' + CLIENT_ID + "&state=" + state);
+  }
+  else if (url.pathname === "/auth/authorize") {
+    console.log(url.searchParams.get('code'));
+    console.log(url.searchParams.get('state'));
+
+    const stateNumbers = atob(url.searchParams.get('state')).split('.');
+    let sum = 13;
+    for (let idx = 0; idx < stateNumbers.length - 1; idx++) {
+      sum += parseInt(randomNumbers[idx]) * idx;
+    }
+
+    const validState = parseInt(stateNumbers[stateNumbers.length - 1]) === sum;
+    console.log('Valid state:', validState);
+    return validState ? new Response("oha vip guest") : new Response("simple middleware");
   }
 
   return new Response("simple middleware");
